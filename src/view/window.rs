d51be0a7a40c4a588c32572614775f7c9a6088c3
csmoe <csmoe@msn.com>
2024-11-27 14:20:58 +0800

    cfg windows

--- src/view/window.rs
+++ src/view/window.rs
@@ -200,9 +200,9 @@ impl Window {
         #[cfg(target_family = "unix")]
         /// See [cef_window_t::get_window_handle]
         fn get_window_handle(&self) -> *mut ::std::os::raw::c_void;
-        #[cfg(target_os = "windows")]
-        /// See [cef_window_t::get_window_handle]
-        fn get_window_handle(&self) -> *mut windows::Win32::Foundation::HWND;
+        //#[cfg(target_os = "windows")]
+        // See [cef_window_t::get_window_handle]
+        //fn get_window_handle(&self) -> *mut windows::Win32::Foundation::HWND;
         /// See [cef_window_t::send_key_press]
         fn send_key_press(&mut self, key_code: i32, event_flags: u32);
         /// See [cef_window_t::send_mouse_move]
