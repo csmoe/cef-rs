6913ea11817117b79f3a729bc016e7d01ee86fc7
csmoe <csmoe@msn.com>
2024-12-23 19:12:00 +0800

    fix demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,6 +1,6 @@
 use cef::{
-    CefApp, CefArgs, CefBrowser, CefBrowserSettings, CefBrowserView, CefClient, CefSettings,
-    CefString, PanelDelegate, ViewDelegate, WindowDelegate,
+    CefApp, CefArgs, CefBrowser, CefBrowserSettings, CefBrowserView, CefClient,
+    CefContextMenuHandler, CefSettings, CefString, PanelDelegate, ViewDelegate, WindowDelegate,
 };
 
 #[derive(Debug, Clone, Copy)]
@@ -17,7 +17,11 @@ struct DemoClient;
 impl CefClient for DemoClient {
     type LifeSpan = ();
     type Render = ();
+    type ContextMenu = ContextMenu;
 }
+
+struct ContextMenu;
+impl CefContextMenuHandler for ContextMenu {}
 
 #[derive(Debug)]
 struct DemoWindow {
@@ -31,10 +35,7 @@ impl ViewDelegate for DemoWindow {
 }
 impl PanelDelegate for DemoWindow {}
 impl WindowDelegate for DemoWindow {
-    fn on_window_created(&self, mut window: cef::CefWindow) {
-        window.get_panel().add_child_view(self.browser_view.view());
-        window.show();
-    }
+    fn on_window_created(&self, mut window: cef::CefWindow) {}
 
     fn on_window_destroyed(&self, _window: cef::CefWindow) {
         cef::quit_message_loop();
@@ -42,29 +43,19 @@ impl WindowDelegate for DemoWindow {
 }
 
 fn main() {
-    //let cef_path = std::path::PathBuf::from(std::env::var("CEF_PATH").unwrap())
-    //    .canonicalize()
-    //    .unwrap();
     let mut args = CefArgs::new(std::env::args());
-    dbg!(&args);
     let app = Application;
     let mut settings = CefSettings::new();
     settings.root_cache_path = CefString::from("/tmp/demo").into();
-    settings.no_sandbox = true;
     cef::execute_process(&mut args, Some(app)).unwrap();
     cef::initialize(&mut args, &settings, Some(app)).unwrap();
 
     let window_info = cef::CefWindowInfo::new();
     let browser_settings = CefBrowserSettings::new();
     let client = DemoClient;
-    let url = CefString::new("https://www.google.com");
+    let url = CefString::new("https://www.example.com");
 
-    let _brower_view =
-        CefBrowserView::create(Some(client), &url, browser_settings.clone()).unwrap();
-    //let delegate = DemoWindow { browser_view };
-
-    //cef::create_top_level_window(delegate);
-    CefBrowser::create(window_info, Some(client), url, browser_settings).unwrap();
+    CefBrowser::create(window_info, Some(client), url, browser_settings, None).unwrap();
 
     cef::run_message_loop();
 
